245c429c3df3393c107e0d6fc885a715c47bbb07
djheidihoe mbp <djheidihoe@gmail.com>
2025-09-05 23:17:45 +0200

    init

---
+++ plain-python-floats2int.py
@@ -0,0 +1,39 @@
+import time
+
+t0 = time.perf_counter()
+file = "data/measurements.txt"
+with open(file) as inputfile:
+    lines = inputfile.readlines()
+    pass
+t1 = time.perf_counter()
+
+stats = {}  # minimum, max, sum
+
+
+def parse_temp(s: str) -> int:
+    neg = s[0] == "-"
+    if neg:
+        s = s[1:]
+    temp = int(s.split(".")[0]) * 10 + int(s.split(".")[1])
+    return -temp if neg else temp
+
+
+for line in lines:
+    city, temp_str = line.split(";")
+    # negative case
+    temp = parse_temp(temp_str)
+    if city in stats:
+        # if min
+        if temp < stats[city][0]:
+            stats[city][0] = temp
+        if temp > stats[city][1]:
+            stats[city][1] = temp
+        stats[city][2] += temp
+        stats[city][3] += 1
+    else:
+        stats[city] = [temp, temp, temp, 1]  # minimum, max, sum
+
+
+t2 = time.perf_counter()
+print("Read:", t1 - t0, "s")
+print("Parse and Aggreagate", t2 - t1, "s")
